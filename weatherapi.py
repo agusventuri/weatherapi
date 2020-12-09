0e8961517b422a85cbcbba13f9d749e16d0d36e3
agusventuri <PCw10SPl502>
2020-12-09 02:01:10 -0300

    creado frontend para card weather y endpoint para obtener dicho weather

--- weatherapi.py
+++ weatherapi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# @app.route('/<int:post_id>')
+# @app.route('/weather?city=<city>&country=<country>&')
+# @app.route('/weather/<city>/<country>')
+@app.route("/weather", methods=['GET'])
+def weather():
+    city = request.args.get('city', None)
+    country = request.args.get('country', None)
+    return render_template('weather.html', city=city, country=country)
