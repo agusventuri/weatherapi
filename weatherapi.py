d0ab49b2531d9b4e7b46dbe416d7e76f1f46b37e
agusventuri <PCw10SPl502>
2020-12-10 02:20:06 -0300

    adding forecast

--- weatherapi.py
+++ weatherapi.py
@@ -38,20 +38,46 @@ def weather():
 
 
 def get_weather(city, country):
-    openweather_json = fetch_openweather_json(city, country)["current"]
-    location_name = openweather_json["city"]["@name"] + ", " + openweather_json["city"]["country"]
-    temperature = openweather_json["temperature"]["@value"] + " " + openweather_json["temperature"]["@unit"]
-    wind_name = openweather_json["wind"]["speed"]["@name"] + ", "
-    wind_speed = openweather_json["wind"]["speed"]["@value"] + openweather_json["wind"]["speed"]["@unit"]
-    wind_direction = ", " + openweather_json["wind"]["direction"]["@name"]
+    openweather_weather = fetch_openweather_weather(city, country)["current"]
+
+    location_name = openweather_weather["city"]["@name"] + ", " + openweather_weather["city"]["country"]
+    temperature = openweather_weather["temperature"]["@value"] + " " + openweather_weather["temperature"]["@unit"]
+
+    wind_name = openweather_weather["wind"]["speed"]["@name"] + ", "
+    wind_speed = openweather_weather["wind"]["speed"]["@value"] + openweather_weather["wind"]["speed"]["@unit"]
+    wind_direction = ", " + openweather_weather["wind"]["direction"]["@name"]
     wind = wind_name + wind_speed + wind_direction
-    cloudiness = openweather_json["clouds"]["@name"]
-    pressure = openweather_json["pressure"]["@value"] + " " + openweather_json["pressure"]["@unit"]
-    humidity = openweather_json["humidity"]["@value"] + openweather_json["humidity"]["@unit"]
-    sunrise = openweather_json["city"]["sun"]["@rise"] + " " + openweather_json["city"]["sun"]["@rise"]
-    sunset = openweather_json["city"]["sun"]["@set"] + " " + openweather_json["city"]["sun"]["@set"]
-    geo_coordinates = "[" + openweather_json["city"]["coord"]["@lat"] + ", " + openweather_json["city"]["coord"]["@lon"] + "]"
+
+    cloudiness = openweather_weather["clouds"]["@name"]
+    pressure = openweather_weather["pressure"]["@value"] + " " + openweather_weather["pressure"]["@unit"]
+    humidity = openweather_weather["humidity"]["@value"] + openweather_weather["humidity"]["@unit"]
+
+    sunrise = openweather_weather["city"]["sun"]["@rise"] + " " + openweather_weather["city"]["sun"]["@rise"]
+    sunset = openweather_weather["city"]["sun"]["@set"] + " " + openweather_weather["city"]["sun"]["@set"]
+
+    lat = openweather_weather["city"]["coord"]["@lat"]
+    lon = openweather_weather["city"]["coord"]["@lon"]
+    geo_coordinates = "[" + lat + ", " + lon + "]"
+
     requested_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    openweather_forecast = fetch_openweather_forecast(lat, lon)
+
+    for forecast in openweather_forecast["daily"]:
+        forecast.pop("dew_point", None)
+        forecast.pop("dt", None)
+        forecast.pop("feels_like", None)
+        forecast.pop("pop", None)
+        forecast.pop("clouds", None)
+        forecast["temp"].pop("morn", None)
+        forecast["temp"].pop("night", None)
+        forecast["temp"].pop("eve", None)
+        forecast["temp"].pop("day", None)
+        forecast.pop("uvi", None)
+        forecast["weather"][0].pop("icon", None)
+        forecast["weather"][0].pop("id", None)
+        forecast["weather"][0].pop("main", None)
+        forecast.pop("wind_deg", None)
 
     weather_json = {
         "location_name": location_name,
@@ -66,11 +92,11 @@ def get_weather(city, country):
         "requested_time": requested_time
                       # "forecast": {...}
     }
-    return weather_json
-    # return openweather_json
+    # return weather_json
+    return openweather_forecast
 
 
-def fetch_openweather_json(city, country):
+def fetch_openweather_weather(city, country):
     q = city + ", " + country
     params = {
         'appid': '1508a9a4840a5574c822d70ca2132032',
@@ -83,5 +109,21 @@ def fetch_openweather_json(city, country):
     xml_response = xmltodict.parse(r.text)
 
     return xml_response
+
+
+def fetch_openweather_forecast(lat, lon):
+    params = {
+        'appid': '1508a9a4840a5574c822d70ca2132032',
+        'exclude': 'current,hourly,minutely,alerts',
+        'units': 'metric',
+        'lat': lat,
+        'lon': lon
+    }
+    # example for daily forecast
+    # https://api.openweathermap.org/data/2.5/onecall?lat=10.46&lon=-73.25&exclude=current,hourly,minutely,alerts&appid=1508a9a4840a5574c822d70ca2132032
+    r = requests.get(
+        'http://api.openweathermap.org/data/2.5/onecall', params=params)
+
+    return r.json()
 
 
