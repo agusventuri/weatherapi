503565b14f5d9ea67546a94d29c319be552beb26
agusventuri <PCw10SPl502>
2020-12-10 23:40:17 -0300

    added some more tests

--- tests/test_weatherapi.py
+++ tests/test_weatherapi.py
@@ -10,16 +10,41 @@ def client():
         yield client
 
 
-def test_no_city(client):
+def test_empty_city(client):
     rv = client.get("/weather?city=&country=co")
     assert rv.json["cod"] == "400"
 
 
-def test_no_country(client):
+def test_empty_country(client):
     rv = client.get("/weather?city=valledupar&country=")
+    assert rv.json["cod"] == "400"
+
+
+def test_no_city(client):
+    rv = client.get("/weather?country=co")
+    assert rv.json["cod"] == "400"
+
+
+def test_no_country(client):
+    rv = client.get("/weather?city=valledupar")
     assert rv.json["cod"] == "400"
 
 
 def test_no_data(client):
     rv = client.get("/weather")
+    assert rv.json["cod"] == "400"
+
+
+def test_country_not_lowercase(client):
+    rv = client.get("/weather?city=valledupar&country=cO")
+    assert rv.json["cod"] == "400"
+
+
+def test_country_less_than_2_chars(client):
+    rv = client.get("/weather?city=valledupar&country=c")
+    assert rv.json["cod"] == "400"
+
+
+def test_country_more_than_2_chars(client):
+    rv = client.get("/weather?city=valledupar&country=col")
     assert rv.json["cod"] == "400"
